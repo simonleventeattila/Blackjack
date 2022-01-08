abc246f489a710065cb9765bf72dfc32f6b0e923
IDEAPAD3\F0RCE <simon.leventeattila@gmail.com>
2022-01-08 17:27:14 +0200

    Added CORS functionality

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+//import fetch from 'node-fetch';
+//import express from 'express'
 const express = require('express')
 const app = express()
-const port = 3000
-const path = require('path')
+const portExpress = 80
+//import path from 'path'
+//const path = require('path')
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+app.use(express.static('public'))
+// app.get('/', (req, res) => {
+// res.sendFile(path.join(__dirname, '/public/index.html'));
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(portExpress, () => {
+  console.log(`Example app listening at http://localhost:${portExpress}`)
+  
 })
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
